b929c83489bb87fd08a9e50174120d6b8ffcbf9f
beisagasaul <beisagasaul21@gmail.com>
2023-10-20 17:56:49 -0600

    CRUD de productos

---
+++ Sis_457SMBJC/ClnSis457/ProductoCl.cs
@@ -0,0 +1,72 @@
+﻿using CadSis457;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClnSis457
+{
+    public class ProductoCl
+    {
+        public static int insertar(Producto producto)
+        {
+            using (var context = new LabSMBJCEntities())
+            {
+                context.Producto.Add(producto);
+                context.SaveChanges();
+                return producto.id;
+            }
+        }
+
+        public static int actualizar(Producto producto)
+        {
+            using (var context = new LabSMBJCEntities())
+            {
+                var existente = context.Producto.Find(producto.id);
+                existente.codigo = producto.codigo;
+                existente.descripcion = producto.descripcion;
+                existente.unidadMedida = producto.unidadMedida;
+                existente.saldo = producto.saldo;
+                existente.precioVenta = producto.precioVenta;
+                existente.usuarioRegistro = producto.usuarioRegistro;
+                return context.SaveChanges();
+            }
+        }
+
+        public static int eliminar(int id, string usuarioRegistro)
+        {
+            using (var context = new LabSMBJCEntities())
+            {
+                var existente = context.Producto.Find(id);
+                existente.estado = -1;
+                existente.usuarioRegistro = usuarioRegistro;
+                return context.SaveChanges();
+            }
+        }
+
+        public static Producto get(int id)
+        {
+            using (var context = new LabSMBJCEntities())
+            {
+                return context.Producto.Find(id);
+            }
+        }
+
+        public static List<Producto> listar()
+        {
+            using (var context = new LabSMBJCEntities())
+            {
+                return context.Producto.Where(x => x.estado != -1).ToList();
+            }
+        }
+
+        public static List<paProductoListar_Result> listarPa(string parametro)
+        {
+            using (var context = new LabSMBJCEntities())
+            {
+                return context.paProductoListar(parametro).ToList();
+            }
+        }
+    }
+}
