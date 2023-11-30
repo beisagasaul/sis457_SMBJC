f909d49c53c3debf96d80bcd2559211adab94637
jhonny condori <jhonnyc0023@gmail.com>
2023-11-30 06:44:58 -0400

    login y template

---
+++ Sis_457SMBJC/Web__SMBJC_MVC/Controllers/CategoriasController.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Web__SMBJC_MVC.Models;
+
+namespace Web__SMBJC_MVC.Controllers
+{
+    public class CategoriasController : Controller
+    {
+        private readonly LabSmbjcContext _context;
+
+        public CategoriasController(LabSmbjcContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Categorias
+        public async Task<IActionResult> Index()
+        {
+              return _context.Categoria != null ? 
+                          View(await _context.Categoria.ToListAsync()) :
+                          Problem("Entity set 'LabSmbjcContext.Categoria'  is null.");
+        }
+
+        // GET: Categorias/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.Categoria == null)
+            {
+                return NotFound();
+            }
+
+            var categorium = await _context.Categoria
+                .FirstOrDefaultAsync(m => m.IdCategoria == id);
+            if (categorium == null)
+            {
+                return NotFound();
+            }
+
+            return View(categorium);
+        }
+
+        // GET: Categorias/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Categorias/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("IdCategoria,Nombre")] Categorium categorium)
+        {
+            if (!string.IsNullOrEmpty(categorium.Nombre) )
+            {
+                categorium.UsuarioRegistro = "sis257";
+                categorium.FechaRegistro = DateTime.Now;
+                categorium.Estado = 1;
+                _context.Add(categorium);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(categorium);
+        }
+
+        // GET: Categorias/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.Categoria == null)
+            {
+                return NotFound();
+            }
+
+            var categorium = await _context.Categoria.FindAsync(id);
+            if (categorium == null)
+            {
+                return NotFound();
+            }
+            return View(categorium);
+        }
+
+        // POST: Categorias/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("IdCategoria,Nombre,UsuarioRegistro,FechaRegistro,Estado")] Categorium categorium)
+        {
+            if (id != categorium.IdCategoria)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(categorium);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!CategoriumExists(categorium.IdCategoria))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(categorium);
+        }
+
+        // GET: Categorias/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.Categoria == null)
+            {
+                return NotFound();
+            }
+
+            var categorium = await _context.Categoria
+                .FirstOrDefaultAsync(m => m.IdCategoria == id);
+            if (categorium == null)
+            {
+                return NotFound();
+            }
+
+            return View(categorium);
+        }
+
+        // POST: Categorias/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.Categoria == null)
+            {
+                return Problem("Entity set 'LabSmbjcContext.Categoria'  is null.");
+            }
+            var categorium = await _context.Categoria.FindAsync(id);
+            if (categorium != null)
+            {
+                _context.Categoria.Remove(categorium);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool CategoriumExists(int id)
+        {
+          return (_context.Categoria?.Any(e => e.IdCategoria == id)).GetValueOrDefault();
+        }
+    }
+}
---
+++ Sis_457SMBJC/Web__SMBJC_MVC/Controllers/VentasController.cs
@@ -0,0 +1,173 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Web__SMBJC_MVC.Models;
+
+namespace Web__SMBJC_MVC.Controllers
+{
+    public class VentasController : Controller
+    {
+        private readonly LabSmbjcContext _context;
+
+        public VentasController(LabSmbjcContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Ventas
+        public async Task<IActionResult> Index()
+        {
+            var labSmbjcContext = _context.Venta.Include(v => v.IdClienteNavigation).Include(v => v.IdUsuarioNavigation);
+            return View(await labSmbjcContext.ToListAsync());
+        }
+
+        // GET: Ventas/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.Venta == null)
+            {
+                return NotFound();
+            }
+
+            var ventum = await _context.Venta
+                .Include(v => v.IdClienteNavigation)
+                .Include(v => v.IdUsuarioNavigation)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (ventum == null)
+            {
+                return NotFound();
+            }
+
+            return View(ventum);
+        }
+
+        // GET: Ventas/Create
+        public IActionResult Create()
+        {
+            ViewData["IdCliente"] = new SelectList(_context.Clientes, "Id", "Id");
+            ViewData["IdUsuario"] = new SelectList(_context.Usuarios, "IdUsuario", "IdUsuario");
+            return View();
+        }
+
+        // POST: Ventas/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,IdUsuario,IdCliente,TotalVenta,FechaVenta,UsuarioRegistro,FechaRegistro,Estado")] Ventum ventum)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(ventum);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["IdCliente"] = new SelectList(_context.Clientes, "Id", "Id", ventum.IdCliente);
+            ViewData["IdUsuario"] = new SelectList(_context.Usuarios, "IdUsuario", "IdUsuario", ventum.IdUsuario);
+            return View(ventum);
+        }
+
+        // GET: Ventas/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.Venta == null)
+            {
+                return NotFound();
+            }
+
+            var ventum = await _context.Venta.FindAsync(id);
+            if (ventum == null)
+            {
+                return NotFound();
+            }
+            ViewData["IdCliente"] = new SelectList(_context.Clientes, "Id", "Id", ventum.IdCliente);
+            ViewData["IdUsuario"] = new SelectList(_context.Usuarios, "IdUsuario", "IdUsuario", ventum.IdUsuario);
+            return View(ventum);
+        }
+
+        // POST: Ventas/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,IdUsuario,IdCliente,TotalVenta,FechaVenta,UsuarioRegistro,FechaRegistro,Estado")] Ventum ventum)
+        {
+            if (id != ventum.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(ventum);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!VentumExists(ventum.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["IdCliente"] = new SelectList(_context.Clientes, "Id", "Id", ventum.IdCliente);
+            ViewData["IdUsuario"] = new SelectList(_context.Usuarios, "IdUsuario", "IdUsuario", ventum.IdUsuario);
+            return View(ventum);
+        }
+
+        // GET: Ventas/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.Venta == null)
+            {
+                return NotFound();
+            }
+
+            var ventum = await _context.Venta
+                .Include(v => v.IdClienteNavigation)
+                .Include(v => v.IdUsuarioNavigation)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (ventum == null)
+            {
+                return NotFound();
+            }
+
+            return View(ventum);
+        }
+
+        // POST: Ventas/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.Venta == null)
+            {
+                return Problem("Entity set 'LabSmbjcContext.Venta'  is null.");
+            }
+            var ventum = await _context.Venta.FindAsync(id);
+            if (ventum != null)
+            {
+                _context.Venta.Remove(ventum);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool VentumExists(int id)
+        {
+          return (_context.Venta?.Any(e => e.Id == id)).GetValueOrDefault();
+        }
+    }
+}
