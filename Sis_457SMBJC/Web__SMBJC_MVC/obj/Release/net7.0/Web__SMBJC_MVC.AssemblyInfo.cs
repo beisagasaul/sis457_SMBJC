27714105aa0ecbfb3a0c8fdbc37498a1db57edb4
beisagasaul <beisagasaul21@gmail.com>
2023-12-01 06:40:16 -0700

    diseño

--- Sis_457SMBJC/Web__SMBJC_MVC/obj/Release/net7.0/Web__SMBJC_MVC.AssemblyInfo.cs
+++ Sis_457SMBJC/Web__SMBJC_MVC/obj/Release/net7.0/Web__SMBJC_MVC.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Web__SMBJC_MVC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+020c437fb2c80dc615346304e23b24df2a2292de")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Web__SMBJC_MVC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Web__SMBJC_MVC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
